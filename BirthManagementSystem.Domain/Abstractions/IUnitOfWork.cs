b852400d843fa5700838b1ddfdad1875a3c78d06
Rafał Zwierzyński <zwierzynski.e@wp.pl>
2024-07-23 13:42:17 +0200

    Dodanie komentarza do IUnitOfWork

--- BirthManagementSystem.Domain/Abstractions/IUnitOfWork.cs
+++ BirthManagementSystem.Domain/Abstractions/IUnitOfWork.cs
@@ -1,6 +1,14 @@
 ﻿namespace BirthManagementSystem.Domain.Abstractions;
 
+/// <summary>
+/// Abstrakcja wzorca UnitOfWork
+/// </summary>
 public interface IUnitOfWork
 {
+    /// <summary>
+    /// Metoda do zapisywania zmian na bazie danych, 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
